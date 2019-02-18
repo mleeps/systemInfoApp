35f94be89c652abb29e6e2da3741af4a1abcf6b7
Mark Leeper <mleeps@gmail.com>
2019-02-17 16:17:47 -1000

    Completed project following YouTube tutorial.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # systemInfoApp
 # systemInfoApp
+Followed An intro To Electron - Desktop Apps with JavaScript by Traversy Media on youtube.
+http://www.youtube.com/watch?v=mr9Mtm_TRpw
---
+++ main.js
@@ -0,0 +1,36 @@
+const{app, BrowserWindow} = require('electron');
+const path = require("path");
+const url = require("url");
+
+let win;
+
+
+function createWindow()
+{
+  win = new BrowserWindow({width:800, heigth:600});
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, "index.html"),
+    protocol: "file",
+    slashes: true
+  }));
+
+  //Open devtools
+  //win.webContents.openDevTools();
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+//Run create window function
+app.on("ready", createWindow);
+
+// Quit when all windows are closed
+app.on("window-all-closed", () =>
+{
+  if(process.platform !== "darwin")
+  {
+    app.quit();
+  }
+})
